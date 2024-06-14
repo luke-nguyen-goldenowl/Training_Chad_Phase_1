0dd7740566625e12233efab08d9a31af012d6f0e
chad-gos <chad.dinh.goldenowl@gmail.com>
2024-06-14 15:52:37 +0700

    Create menu, handle exercise 1

--- dart_challenge/lib/dart_challenge.dart
+++ dart_challenge/lib/dart_challenge.dart
@@ -1,3 +1,42 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'package:dart_challenge/exercise_1/exercise_1.dart';
+
+void runMenu() {
+  bool run = true;
+  while (run) {
+    stdout.write('\n\nDART CHALLENGES MENU\n');
+    stdout.write('1. Exercise 1\n');
+    stdout.write('0. Exit\n');
+
+    int chooseNumber;
+    while (true) {
+      stdout.write('\n\nPLEASE ENTER THE NUMBER OF YOUR OPTION: ');
+      chooseNumber = int.parse(stdin.readLineSync()!);
+      if (chooseNumber > 1 || chooseNumber < 0) {
+        stdout.write('Please enter again(choose 0 - 1)');
+      } else {
+        break;
+      }
+    }
+    switch (chooseNumber) {
+      case 0:
+        run = false;
+        stdout.write('Exit');
+        break;
+      case 1:
+        exercise1();
+        break;
+      default:
+        stdout.write('Please enter again(choose 0 - 1)');
+    }
+    if (run) {
+      int exit;
+      while (true) {
+        stdout.write('\nDo you want to continue? 1. Yes   0. No\n');
+        exit = int.parse(stdin.readLineSync()!);
+        if (exit == 0 || exit == 1) break;
+      }
+      if (exit == 0) break;
+    }
+  }
 }
---
+++ dart_challenge/lib/exercise_1/exercise_1.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void exercise1() {
+  int size;
+  while (true) {
+    stdout.write('\n\nExercise 1');
+    stdout.write('\nEnter the size of the square: ');
+    size = int.parse(stdin.readLineSync()!);
+    if (size < 1) {
+      stdout.write('Please enter a valid value!');
+    } else {
+      break;
+    }
+  }
+
+  stdout.write('\nRESULT:\n');
+
+  for (int i = 0; i <= size; i++) {
+    for (int j = 0; j <= 2 * size; j++) {
+      if (j < size) {
+        stdout.write(' ---');
+      } else {
+        if (j == size) stdout.write('\n');
+        if (i != size) stdout.write('|   ');
+      }
+    }
+    if (i != size) stdout.write('\n');
+  }
+}
