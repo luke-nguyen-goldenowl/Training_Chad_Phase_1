9f10762032fbf9f00ac6cb71df1b8a1a6461afa3
chad-gos <chad.dinh.goldenowl@gmail.com>
2024-06-21 09:01:20 +0700

    [Fix] Create variables for commonly used logic

--- flutter_gallery_app/lib/feature/home/home_screen.dart
+++ flutter_gallery_app/lib/feature/home/home_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gallery_app/constants/constants.dart';
+import 'package:flutter_gallery_app/feature/home/widgets/list_tile_item.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> listViewData = Constants.listViewData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,44 +34,12 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: ListView.builder(
-        itemCount: Constants.listViewData.length,
-        itemBuilder: (context, index) => GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Constants.listViewData[index]['page'],
-              ),
-            );
-          },
-          child: ListTile(
-            contentPadding: const EdgeInsets.only(left: 20),
-            title: Text(
-              Constants.listViewData[index]['title'],
-              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-            ),
-            subtitle: Container(
-              padding: const EdgeInsets.only(bottom: 10, right: 10),
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                      color: Color.fromARGB(255, 220, 220, 220), width: 1.0),
-                ),
-              ),
-              child: Text(
-                Constants.listViewData[index]['subtitle'],
-                style: const TextStyle(fontSize: 14, height: 1.2),
-              ),
-            ),
-            leading: Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: Icon(
-                Constants.listViewData[index]['leading'],
-                color: const Color.fromARGB(255, 120, 120, 120),
-              ),
-            ),
-          ),
-        ),
+        itemCount: listViewData.length,
+        itemBuilder: (context, index) {
+          return ListTileItem(
+            itemData: listViewData[index],
+          );
+        },
       ),
     );
   }
